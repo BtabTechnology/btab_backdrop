a89f2b678195d52fe0cbe86cb2fd8f5d4169d7f7
Steve Deals <6135415+SteveDeals@users.noreply.github.com>
2024-10-09 18:50:31 +1100

    Revert "Featurex - Second Commit (#16)" (#17)
    This reverts commit 68af064564d86dc9b67ac1b64c3a49068276b036.

--- btab-store/src/repositories/vendor.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Vendor } from "../models/vendor";
-
-@EntityRepository(Vendor)
-export class VendorRepository extends Repository<Vendor> {
-  // You can add custom methods here if needed
-}
